5edff855f19f0802653bf44cd56a1c4958ab1f9f
Rina Toyoshiba <rtoyoshiba@gmail.com>
2019-06-06 22:37:54 -1000

    finish

--- basics.js
+++ basics.js
@@ -16,16 +16,32 @@ var myName = 'Rina Toyoshiba';
  * if it should be true if your person object is at least 16 years old
  */
 
+ var canDrive = true;
+
 /* Create a function called `greet`,
  * it should take a 1 parameter, `name`
  * and it should print "Hello, my name is {name}"
  */
 
+ function greet(name){
+     console.log('Hello, my name is '+ name);
+ }
+ greet('Rina');
+
 /* Create an array called `dataTypes` with atleast 1 of every data type;
  * (there are 6 different data types);
  */
+
+var dataTypes = ['ice cream', 8, false, {animal:'dog', breed:'shiba inu'}, ['blue', 'purple'], undefined, null];
 
 /* Create a `dog` object
  * it should have a `bark` function that makes your dog bark!
  * It should also have a name attribute with the value of 'Spot'
  */
+
+ var dog = {
+     name: 'Spot',
+     bark: function(){
+         console.log('bark!');
+     }
+ }
